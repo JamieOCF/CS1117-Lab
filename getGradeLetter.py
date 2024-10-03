b1e0c0e153cdf8ffdc01ee3ee808cf1ffa1074d6
Jamie <jamie.o.foley86@gmail.com>
2024-10-03 12:33:10 +0100

    Add files via upload

---
+++ getGradeLetter.py
@@ -0,0 +1,21 @@
+gradePercentage = int(input("Enter grade percentage in numerical form: "))
+
+if gradePercentage >100 or gradePercentage <0:
+    print ("X")
+elif gradePercentage >=85:
+    print ("A")
+elif gradePercentage >70:
+    print ("B")
+elif gradePercentage >=55:
+    print ("C")
+elif gradePercentage >=40:
+    print ("D")
+elif gradePercentage >=25:
+    print ("E")
+elif gradePercentage >=0:
+    print ("F")
+
+
+
+
+
