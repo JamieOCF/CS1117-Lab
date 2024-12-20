71feb12bcba51bd50da134e3b1a48dcf5319d695
Jamie <jamie.o.foley86@gmail.com>
2024-12-20 23:39:52 +0000

    payslip.py

--- payslip.py
+++ payslip.py
@@ -1,42 +1,42 @@
-# #Ask for hours
-# hours = int(input("How many hours did you work this week? "))
-# rate = 12
-# ot_rate = 20
+#Ask for hours
+hours = int(input("How many hours did you work this week? "))
+rate = 12
+ot_rate = 20
 
-# #Other
-# basic_hours = 40
-# ot_hours = hours - basic_hours
+#Other
+basic_hours = 40
+ot_hours = hours - basic_hours
 
-# ot_pay = ot_hours * ot_rate
-# b_pay = basic_hours * rate
-# pay = ot_pay + b_pay
-
-
-# #Payslip w/10 hours OT
-# print ("Hours worked:",hours)
-# print ("Basic Pay:",b_pay)
-# print ("Overtime Pay:",ot_pay)
-# print ("Total Pay:",pay)
+ot_pay = ot_hours * ot_rate
+b_pay = basic_hours * rate
+pay = ot_pay + b_pay
 
 
+#Payslip w/10 hours OT
+print ("Hours worked:",hours)
+print ("Basic Pay:",b_pay)
+print ("Overtime Pay:",ot_pay)
+print ("Total Pay:",pay)
 
 
 
 
 
-# def payslip (hrs):
-#     overtime = hours - 40
-#     rate = 12
-#     ot_rate = 20
-#     bp = hours * rate
-#     otp = overtime * ot_rate
-#     tp = bp + otp
-#     return bp, otp, tp
 
-# hours = int(input("How many hours did you work this week? "))
-# overtime = hours - 40
 
-# basic_pay, overtime_pay, weekly_total_pay = payslip (hours)
-# print ("Basic Pay:",basic_pay)
-# print ("Overtime Pay:",overtime_pay)
-# print ("Total Pay:",weekly_total_pay)
+def payslip (hrs):
+    overtime = hours - 40
+    rate = 12
+    ot_rate = 20
+    bp = hours * rate
+    otp = overtime * ot_rate
+    tp = bp + otp
+    return bp, otp, tp
+
+hours = int(input("How many hours did you work this week? "))
+overtime = hours - 40
+
+basic_pay, overtime_pay, weekly_total_pay = payslip (hours)
+print ("Basic Pay:",basic_pay)
+print ("Overtime Pay:",overtime_pay)
+print ("Total Pay:",weekly_total_pay)
