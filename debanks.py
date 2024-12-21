10170aba7a9bf53b0dad00ec8ac3d8d2eded126f
Jamie <jamie.o.foley86@gmail.com>
2024-12-21 00:06:12 +0000

    debanks.py

--- debanks.py
+++ debanks.py
@@ -1,49 +1,49 @@
 #a
-# inFile = open("debanks.txt","r")
-# lyrics = inFile.readline()
-# line = 0
-# while lyrics != "":
-#     line += 1
-#     lyric = lyrics.strip("\n")
-#     print (f"{line}:{lyric}")
-#     lyrics = inFile.readline()
-# inFile.close()
+inFile = open("debanks.txt","r")
+lyrics = inFile.readline()
+line = 0
+while lyrics != "":
+    line += 1
+    lyric = lyrics.strip("\n")
+    print (f"{line}:{lyric}")
+    lyrics = inFile.readline()
+inFile.close()
 
 
 #b
-# inFile = open("debanks.txt", "r")
+inFile = open("debanks.txt", "r")
 
-# lyrics = inFile.read()
-# lyric = lyrics.split("\n")
+lyrics = inFile.read()
+lyric = lyrics.split("\n")
 
-# for i in range(len(lyric)):
-#     if i == 12:
-#         exit()
-#     else:
-#         print (f"{i+1}:{lyric[i]}")
-# inFile.close()
+for i in range(len(lyric)):
+    if i == 12:
+        exit()
+    else:
+        print (f"{i+1}:{lyric[i]}")
+inFile.close()
 
 
 #c
-# inFile = open("debanks.txt", "r")
+inFile = open("debanks.txt", "r")
 
-# lyrics = inFile.readlines()
-# line = 0
-# for lyric in lyrics:
-#     lyric = lyric.strip("\n")
-#     line += 1
-#     print (f"{line}:{lyric}")
-# inFile.close()
+lyrics = inFile.readlines()
+line = 0
+for lyric in lyrics:
+    lyric = lyric.strip("\n")
+    line += 1
+    print (f"{line}:{lyric}")
+inFile.close()
 
 
 #d
-# inFile = open("debanks.txt", "r")
-# line = 0
-# for lyric in inFile:
-#     line += 1
-#     lyric = lyric.strip("\n")
-#     print (f"{line}:{lyric}")
-# inFile.close()
+inFile = open("debanks.txt", "r")
+line = 0
+for lyric in inFile:
+    line += 1
+    lyric = lyric.strip("\n")
+    print (f"{line}:{lyric}")
+inFile.close()
 
 
 
